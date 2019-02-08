6d2b38a452f95d6afd37ef9085aa9f3e87d4aa98
Lokeswari Pittu <s531378@nwmissouri.edu>
2019-02-08 12:53:52 -0600

    Commit

---
+++ src/Web/Controllers/QuizController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class QuizController : Controller
+    {
+        public IActionResult ShowText()
+        {
+            return Content("Here is the Quiz. ");
+        }
+        public IActionResult ShowJSON()
+        {
+            return Json(new { name = "new Question", quote = "This is my first Quiz." });
+        }
+        public IActionResult ShowHTML()
+        {
+            var h = "<!DOCTYPE html><html><body>Question 1. .NET Core is designed to integrate seamlessly with a variety of client-side frameworks, such as AngularJS, React, and Vue. </body></html>";
+            return Content(h, "text/html");
+        }
+
+        public IActionResult ShowView()
+        {
+            return View();
+        }
+    }
+}
+
--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -6,9 +6,34 @@ namespace UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void FactorialOfANumber()
         {
+            var expected = 24;
+            var a = 4;
+            var actual = Factorial(a);
+            Assert.Equal(expected,actual);
 
         }
+       
+ [Theory]
+        [InlineData(2,2)]
+        [InlineData(120, 5)]
+        [InlineData(1, 0)]
+        public void Factorial_WorksWithManyInputs(int exp, int n1)
+        {
+            Assert.Equal(exp, Factorial(n1));
+        }
+        
+        int Factorial(int x)
+        {
+           if (x == 0) 
+                {
+                 return 1; 
+                }
+              else{
+          return x*Factorial(x-1); 
+                }
+
+                }
     }
 }
